50053a0e12d7704930b65df87eb5ed0d664f045a
DamianLubkowski <multiclick@wp.pl>
2022-09-21 18:37:07 +0200

    zrealizowany punkt 7d

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -3,10 +3,12 @@ package pl.lublin.wsei.java.cwiczenia;
 public class Main {
 
     public static void main(String[] args) {
+        int a = 3;
+        double b = 4.21;
+        String s = "jakiś tekst";
 
-        System.out.print("Ala\n");
-        System.out.print("ma\n");
-        System.out.print("kota\n");
+        System.out.printf("a = %d, b = %f, s = %s %n", a, b, s);
+
 
     }
 }
