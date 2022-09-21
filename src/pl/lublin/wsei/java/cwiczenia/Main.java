54b8996dc1af1d330225fca5c900073461161d04
DamianLubkowski <multiclick@wp.pl>
2022-09-21 18:29:46 +0200

    zrealizowany punkt 7a

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -3,9 +3,9 @@ package pl.lublin.wsei.java.cwiczenia;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Ala");
-        System.out.print("ma");
-        System.out.print("kota");
+        System.out.println("Ala");
+        System.out.println("ma");
+        System.out.println("kota");
 
     }
 }
