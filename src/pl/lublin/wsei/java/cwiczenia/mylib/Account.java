bd835f78b1815eefb5fe271a32b538d4f066b060
DamianLubkowski <multiclick@wp.pl>
2022-09-21 19:41:30 +0200

    zrealizowany punkt 12g

--- src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
@@ -1,13 +1,15 @@
 package pl.lublin.wsei.java.cwiczenia.mylib;
 
-public class Account {
-    public String name;
+import org.apache.commons.lang3.StringUtils;
 
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
+public class Account {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
