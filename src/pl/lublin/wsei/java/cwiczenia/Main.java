beb63c8e4bfc1a2c406d2835c248f7ff1b88279f
DamianLubkowski <multiclick@wp.pl>
2022-09-21 19:04:01 +0200

    zrealizowany punkt 9e

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -9,12 +9,10 @@ public class Main {
         int num1 = 0, num2 = 0;
 
         do {
-            System.out.print("Podaj pierwszą liczbę: ");
+            System.out.print("Podaj liczbę, którą mam dodać: ");
             num1 = input.nextInt();
-            if (num1 == 0) break;
-            System.out.print("Podaj drugą liczbę: ");
             num2 = input.nextInt();
-            if (num2 == 0) break;
+            if ((num1 == 0) || (num2 == 0)) break;
             System.out.printf("Wynik dodawania %d + %d = %d%n", num1, num2, num1 + num2);
         } while (true);
     }
